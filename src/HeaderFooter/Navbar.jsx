05c0bb93dcdff67c92d9b820035f45eca10add94
AdityaTheJaat <programmingID1306@gamil.com>
2024-05-24 13:06:34 +0530

    Navbar Fixed

--- src/HeaderFooter/Navbar.jsx
+++ src/HeaderFooter/Navbar.jsx
@@ -1,10 +1,26 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.webp";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [isNavbarFixed, setNavbarFixed] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setNavbarFixed(true);
+      } else {
+        setNavbarFixed(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <div>
-      <nav className="flex justify-between items-center mx-12 font-serif">
+      <nav role="navigation" className={`flex justify-between items-center mx-12 font-serif ${isNavbarFixed ? "fixed top-0 left-0 right-0 bg-white z-50 shadow-lg transition-all" : "relative"}`}>
         <img src={logo} className="m-5 w-44"></img>
         <ul className="list-none w-2/5 flex justify-evenly text-lg font-medium cursor-pointer">
           <Link className="hover:text-cyan-600" to="/" >Home</Link>
