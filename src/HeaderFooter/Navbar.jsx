24782b747cd55699db8bf7ffdb97401ff62e63f7
AdityaTheJaat <programmingID1306@gamil.com>
2024-05-27 22:24:19 +0530

    Navbar fixed

--- src/HeaderFooter/Navbar.jsx
+++ src/HeaderFooter/Navbar.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.webp";
 import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faBars,faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
+  const [shownav,setShownav]=useState(false);
   const [isNavbarFixed, setNavbarFixed] = useState(false);
 
   useEffect(() => {
@@ -20,15 +23,49 @@ const Navbar = () => {
   }, []);
   return (
     <div>
-      <nav role="navigation" className={`flex justify-between items-center mx-12 font-serif ${isNavbarFixed ? "fixed top-0 left-0 right-0 bg-white z-50 shadow-lg transition-all" : "relative"}`}>
-        <img src={logo} className="m-5 w-44"></img>
-        <ul className="list-none w-2/5 flex justify-evenly text-lg font-medium cursor-pointer">
+      <div
+				className={
+					!shownav
+						? "w-screen h-screen bg-black opacity-85 z-50 fixed left-[1800px] transition-all"
+						: "w-screen h-screen bg-black opacity-85 z-50 fixed left-0 transition-all"
+				}>
+				<div className="mt-6 ml-6 " onClick={() => setShownav(!shownav)}>
+					<FontAwesomeIcon
+						icon={faXmark}
+						size="2xl"
+						style={{ color: "#fafcff" }}
+					/>
+				</div>
+				<ul className="list-none h-[470px] items-center flex flex-col justify-evenly text-white text-2xl font-medium cursor-pointer">
+					<Link className="hover:text-cyan-600" to="/" onClick={() => setShownav(!shownav)}>
+						Home
+					</Link>
+					<Link className="hover:text-cyan-600" to="/about" onClick={() => setShownav(!shownav)}>
+						About Us
+					</Link>
+					<Link className="hover:text-cyan-600" to="/team" onClick={() => setShownav(!shownav)}>
+						Team
+					</Link>
+					<Link className="hover:text-cyan-600" to="/event" onClick={() => setShownav(!shownav)}>
+						Events
+					</Link>
+					<Link className="hover:text-cyan-600" to="/contact" onClick={() => setShownav(!shownav)}>
+						Contact Us
+					</Link>
+				</ul>
+			</div>
+      <nav role="navigation" className={`z-10 flex justify-between items-center mx-12 font-serif ${isNavbarFixed ? "fixed top-0 left-0 right-0 bg-white z-50 shadow-lg transition-all" : "relative"}`}>
+        <img src={logo} className="m-5 w-44 max-sm:w-40 max-sm:ml-[-20px]"></img>
+        <ul className="list-none w-2/5 flex justify-evenly text-lg font-medium cursor-pointer max-xl:hidden">
           <Link className="hover:text-cyan-600" to="/" >Home</Link>
           <Link className="hover:text-cyan-600" to="/about">About Us</Link>
           <Link className="hover:text-cyan-600" to="/team">Team</Link>
           <Link className="hover:text-cyan-600" to="/event">Events</Link>
           <Link className="hover:text-cyan-600" to="/contact">Contact Us</Link>
         </ul>
+        <div className="xl:hidden mr-5" onClick={() => setShownav(!shownav)}>
+					<FontAwesomeIcon icon={faBars} size="2xl" />
+				</div>
       </nav>
       <hr />
     </div>
--- src/Home/HomeImg.jsx
+++ src/Home/HomeImg.jsx
@@ -8,9 +8,9 @@ const HomeImg = () => {
         style={{ backgroundImage: `url(${Bgimg})` }}>
       </div>
       <div className="relative bottom-[490px] left-[350px] text-white flex-col flex justify-center items-center w-[850px]">
-        <h1 className="font-bold text-5xl  border-white border-b-2 flex justify-center w-[850px] pb-5 mb-5 font-serif">IEEE Student Branch, LNMIIT</h1>
-        <p className="font-normal text-2xl mb-8">Creating Technology for people and people for technology</p>
-        <button className="border-2 w-40 h-14 font-semibold font-serif text-lg hover:bg-white hover:text-black">Contact Us</button>
+        <h1 className="font-bold text-5xl hidden lg:visible border-white border-b-2 text-center w-[850px] pb-5 mb-5 font-serif">IEEE Student Branch, LNMIIT</h1>
+        <p className="font-normal text-2xl mb-8 hidden lg:visible">Creating Technology for people and people for technology</p>
+        <button className="border-2 w-40 h-14 font-semibold font-serif text-lg hover:bg-white hover:text-black hidden lg:visible">Contact Us</button>
       </div>
     </div>
   )
