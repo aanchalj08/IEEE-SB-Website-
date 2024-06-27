e3faa9bdbf2887bf27c9e50a05b254cb5aabd0f6
Rohitrsr1408 <rohitrana8041@gmail.com>
2024-06-27 11:46:52 +0530

    Contact Us Updated

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import { useForm } from 'react-hook-form';
-
+import contactUS from '../assets/contactUS.jpg'
 const Contact = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
 
@@ -9,16 +9,17 @@ const Contact = () => {
   };
 
   return (
-    <div className="flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
-        <div className='bg-indigo-600 text-white p-4 w-full rounded-2xl'>
-          <h2 className="text-center text-3xl font-extrabold">Contact us</h2>
-          <p className="mt-2 text-center text-sm">
+    <div className="w-[900px] h-[600px] mt-16 mb-28 ml-96 max-xl:ml-20 max-lg:ml-0 max-sm:h-[650px] max-md:w-[600px] max-sm:w-[430px] flex justify-center items-center bg-cover bg-no-repeat bg-center  rounded-lg brightness-[120%]" style={{ backgroundImage: `url(${contactUS})` }}>
+      <div className="w-[70%]  flex " >
+        <div className='h-[500px] ml-4'> 
+        <div className='p-5 mt-[-10px]'>
+          <h2 className="text-5xl text-white font-extrabold pb-3 mt-[-10px] ml-12 max-sm:text-3xl max-sm:ml-6  ">Contact us☎️</h2>
+          <p className="text-lg text-white mb-[-10px] font-semi-bold">
             For more details and answers to your queries, contact us!
           </p>
         </div>
-        <form className="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
-          <div className="rounded-md shadow-sm -space-y-px">
+        <form className="mt-4 space-y-6 " onSubmit={handleSubmit(onSubmit)}>
+          <div className="rounded-md shadow-sm -space-y-px ">
             <div>
               <label htmlFor="name" className="sr-only">Name</label>
               <input 
@@ -27,7 +28,7 @@ const Contact = () => {
                 type="text" 
                 autoComplete="name" 
                 {...register('name')}
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
+                className="appearance-none rounded-none h-12 relative block w-full px-3 py-2 bg-gray-200  border border-gray-700 placeholder-gray-500 text-gray-900 rounded-t-lg focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
                 placeholder="Name" 
               />
             </div>
@@ -39,10 +40,10 @@ const Contact = () => {
                 type="email" 
                 autoComplete="email" 
                 {...register('email', { required: true })}
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
+                className="appearance-none rounded-none h-12 relative block w-full px-3 py-2 bg-gray-200 border border-gray-700 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
                 placeholder="Email" 
               />
-              {errors.email && <span className="text-red-500 text-sm">Email is required</span>}
+
             </div>
             <div>
               <label htmlFor="subject" className="sr-only">Subject</label>
@@ -52,7 +53,7 @@ const Contact = () => {
                 type="text" 
                 autoComplete="subject" 
                 {...register('subject')}
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
+                className="appearance-none rounded-none h-12 relative block w-full px-3 py-2 bg-gray-200 border border-gray-700 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
                 placeholder="Subject" 
               />
             </div>
@@ -63,23 +64,27 @@ const Contact = () => {
                 name="message" 
                 rows="4"
                 {...register('message')}
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
+                className="appearance-none rounded-none relative block w-full px-3 py-2 bg-gray-200 border border-gray-700 placeholder-gray-500 text-gray-900 rounded-b-lg focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
                 placeholder="Message" 
               />
             </div>
           </div>
+          {errors.email && <span className="text-red-500 text-sm ">Email is required</span>}
           <div>
             <button 
               type="submit" 
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-xl font-semibold rounded-lg items-center text-white h-16 bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Send Message
             </button>
           </div>
-          <div className="text-center text-sm text-gray-500">
+          
+          <div className="text-center text-sm text-gray-300 ">
             This site is protected by reCAPTCHA and the Google <a href="https://policies.google.com/privacy" className="underline">Privacy Policy</a> and <a href="https://policies.google.com/terms" className="underline">Terms of Service</a> apply.
           </div>
+         
         </form>
+        </div>
       </div>
     </div>
   )
