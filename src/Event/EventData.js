60b9caeeded42b27f43255bf1e84ca640faa5073
AdityaTheJaat <programmingID1306@gamil.com>
2024-07-01 12:54:51 +0530

    Event Page updates completed

--- src/Event/EventData.js
+++ src/Event/EventData.js
@@ -1,19 +1,27 @@
-import img1 from "../assets/Caro3.jpg";
+import img1 from "../assets/hackph.jpg";
+import img2 from "../assets/blueclue.jpg";
+import img3 from "../assets/robothon.jpg";
+import img4 from "../assets/crack.jpg";
 
 export const EventCardContent = [
   {
     photo: img1,
-    title: "This is Title",
-    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. A.",
+    title: "Web Dev Hackathon",
+    content: "IEEE Student's Branch conducted an exciting event from 11-13 Sep at LT16.",
   },
   {
-    photo: img1,
-    title: "This is Title",
-    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. A.",
+    photo: img2,
+    title: "Blue's Clues",
+    content: "Diving into the unexpected side of IEEE! Beyond electronics and coding, we brought you a fun event ‘Blue’s Clues’ you never saw coming.",
   },
   {
-    photo: img1,
-    title: "This is Title",
-    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. A.",
+    photo: img3,
+    title: "Robothon",
+    content: "Assigned with a unique theme and armed with a collection of electronic components, including sensors and microcontrollers, teams will embark on the task of building a fully functional electronic model. 🛠️",
+  },
+  {
+    photo: img4,
+    title: "Crack the Code 2.0",
+    content: "Only the most daring and cunning participants will unlock the cryptic riches hidden within.",
   },
 ];
